ee969027bcc66105b24e76edcf1fbd4ea86db7bc
sahilfigar <sahilfigar3@gmail.com>
2026-04-29 19:47:50 +0530

    Fix: always sync PostgreSQL password on re-deploy

--- deploy.sh
+++ deploy.sh
@@ -124,6 +124,8 @@ systemctl start postgresql
 log "Configuring PostgreSQL..."
 sudo -u postgres psql -c "SELECT 1 FROM pg_roles WHERE rolname='$DB_USER'" | grep -q 1 || \
   sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';"
+# Always update password to match this deploy's .env
+sudo -u postgres psql -c "ALTER USER $DB_USER WITH PASSWORD '$DB_PASS';"
 sudo -u postgres psql -c "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" | grep -q 1 || \
   sudo -u postgres psql -c "CREATE DATABASE $DB_NAME OWNER $DB_USER;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
